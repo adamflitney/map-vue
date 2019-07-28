5c66e38a285dbac9cd7f7f1bed85f3bc7f7ff20f
adamflitney <adam.flitney@gmail.com>
2019-07-28 16:26:10 +0100

    added marker list to MapComponent

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="map-component">
-        Map Component
+        <div class="map-container" ref="map"></div>
+        <div class="location-list">
+            <ul>
+                <button >
+                    <li
+                    v-for="marker in markers"
+                    :key="marker.title"
+                    @click="selectLocation(marker)"
+                    :class="{ selected: isSelected(marker) }"
+                    >{{marker.title}}</li>
+                </button>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -21,7 +33,8 @@ export default {
       google: null,
       geocoder: null,
       map: null,
-      markers: null
+      markers: null,
+      selectedMarker: null
     }
   },
   watch: {
@@ -31,11 +44,13 @@ export default {
       }
     }
   },
+  computed: {
+
+  },
   methods: {
     updateMarkers () {
       try {
         const markerClickHandler = marker => {
-          this.map.setZoom(13)
           this.map.setCenter(marker.getPosition())
         }
 
@@ -46,7 +61,8 @@ export default {
                 lat: location.latitude,
                 lng: location.longitude
               },
-              map: this.map })
+              map: this.map,
+              title: location.name })
             marker.addListener('click', () => {
               markerClickHandler(marker)
               const infoWindow = new this.google.maps.InfoWindow({
@@ -60,13 +76,24 @@ export default {
       } catch (error) {
         console.error(error)
       }
+    },
+    selectLocation (marker) {
+      this.selectedMarker = marker
+      this.map.setCenter(marker.getPosition())
+    },
+    isSelected (marker) {
+      if (this.selectedMarker) {
+        return marker.title === this.selectedMarker.title
+      } else {
+        return false
+      }
     }
   },
   async mounted () {
     try {
       this.google = await gmapsInit()
       this.geocoder = new this.google.maps.Geocoder()
-      this.map = new this.google.maps.Map(this.$el)
+      this.map = new this.google.maps.Map(this.$refs.map)
 
       this.geocoder.geocode({ address: 'United Kingdom' }, (results, status) => {
         // console.log(status, results)
@@ -88,5 +115,41 @@ export default {
 <style>
 .map-component {
     height: 100%;
+    display: grid;
+    grid-template-columns: 1fr fit-content(20%);
 }
+
+.map-container {
+    height: 100%;
+}
+
+.location-list {
+    max-height: 100vh;
+    overflow-y: scroll;
+}
+
+.location-list ul {
+    width: fit-content;
+    padding: 0;
+    margin: 0;
+}
+
+.location-list li {
+    list-style: none;
+    padding: 1rem 2rem;
+    text-align: center;
+}
+
+.location-list li:hover,
+.selected {
+    background-color: grey;
+}
+
+button {
+    background-color: #fff;
+    border-style: none;
+    padding: 0;
+    margin: 0;
+}
+
 </style>
