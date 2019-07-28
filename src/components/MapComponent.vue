46fab19894e87df8d73016b8fb56c9590909e233
adamflitney <adam.flitney@gmail.com>
2019-07-28 11:50:18 +0100

    created basic component structure

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="map-component">
+        Map Component
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MapComponent'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/pages/MapPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="map-page">
+        <MapComponent />
+    </div>
+</template>
+
+<script>
+import MapComponent from '../MapComponent'
+export default {
+  name: 'MapPage',
+  components: {
+    MapComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
