f1a5bc722cf809617e9d39278ce04e3e027f39e7
adamflitney <adam.flitney@gmail.com>
2019-07-28 12:09:36 +0100

    map data feed connected to vuex store

---
+++ src/api/mapDataAPI.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const endpointURI = 'https://s3-eu-west-1.amazonaws.com/omnifi/techtests/locations.json'
+
+export function getMapData () {
+  return axios.get(endpointURI)
+    .then(response => {
+      return response
+    })
+    .catch(error => {
+      return error
+    })
+}
--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { getMapData } from './api/mapDataAPI'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    locationData: []
   },
   mutations: {
-
+    updateLocationData (state, newData) {
+      state.locationData = newData
+    }
   },
   actions: {
-
+    getLatestLocationData ({ commit }) {
+      const newMapData = getMapData()
+      commit('updateLocationData', newMapData)
+    }
   }
 })
