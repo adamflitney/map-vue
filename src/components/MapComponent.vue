e6876fb5c9886c534b3cdc36f90568f910aa0c62
adamflitney <adam.flitney@gmail.com>
2019-07-28 12:33:46 +0100

    added google maps functionality - auth rejected currently

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -5,10 +5,28 @@
 </template>
 
 <script>
+import gmapsInit from '../utils/gmaps'
 export default {
   name: 'MapComponent',
-  mounted () {
+  async mounted () {
     this.$store.dispatch('getLatestLocationData')
+
+    try {
+      const google = await gmapsInit()
+      const geocoder = new google.maps.Geocoder()
+      const map = new google.maps.Map(this.$el)
+
+      geocoder.geocode({ address: 'Austria' }, (results, status) => {
+        if (status !== 'OK' || !results[0]) {
+          throw new Error(status)
+        }
+
+        map.setCenter(results[0].geometry.location)
+        map.fitBounds(results[0].geometry.viewport)
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 </script>
---
+++ src/utils/gmaps.js
@@ -0,0 +1,40 @@
+// This is taken directly from: https://markus.oberlehner.net/blog/using-the-google-maps-api-with-vue/
+// It seemed easier and cleaner than manually adding the script to the html file
+
+const API_KEY = 'AIzaSyAMombimZAzqw1HZOf3AfeIKj3u6gv7UcU'
+const CALLBACK_NAME = 'gmapsCallback'
+
+let initialized = !!window.google
+let resolveInitPromise
+let rejectInitPromise
+// This promise handles the initialization
+// status of the google maps script.
+const initPromise = new Promise((resolve, reject) => {
+  resolveInitPromise = resolve
+  rejectInitPromise = reject
+})
+
+export default function init () {
+  // If Google Maps already is initialized
+  // the `initPromise` should get resolved
+  // eventually.
+  if (initialized) return initPromise
+
+  initialized = true
+  // The callback function is called by
+  // the Google Maps script if it is
+  // successfully loaded.
+  window[CALLBACK_NAME] = () => resolveInitPromise(window.google)
+
+  // We inject a new script tag into
+  // the `<head>` of our HTML to load
+  // the Google Maps script.
+  const script = document.createElement('script')
+  script.async = true
+  script.defer = true
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=${CALLBACK_NAME}`
+  script.onerror = rejectInitPromise
+  document.querySelector('head').appendChild(script)
+
+  return initPromise
+}
